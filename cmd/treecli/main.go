2d80ce3a0a425e231245cd3505f29ddaee1220da
Luciano <lucianorodriguess101@gmail.com>
2024-10-19 13:58:29 -0300

    refactor(cli): Update main function to handle exclude flag and pass exclude globs to TreeService

--- cmd/treecli/main.go
+++ cmd/treecli/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
+	"strings"
 
 	"treecli/internal/application"
 	"treecli/internal/infrastructure"
@@ -10,13 +12,25 @@ import (
 )
 
 func main() {
+	exclude := flag.String("exclude", "", "Padrões de exclusão (wildcards), separados por vírgula")
+	flag.Parse()
+
 	path := "."
-	if len(os.Args) > 1 {
-		path = os.Args[1]
+	args := flag.Args()
+	if len(args) > 0 {
+		path = args[0]
+	}
+
+	var excludeGlobs []string
+	if *exclude != "" {
+		excludeGlobs = strings.Split(*exclude, ",")
+		for i, pattern := range excludeGlobs {
+			excludeGlobs[i] = strings.TrimSpace(pattern)
+		}
 	}
 
 	fsRepo := infrastructure.NewFileSystemRepository()
-	treeService := application.NewTreeService(fsRepo)
+	treeService := application.NewTreeService(fsRepo, excludeGlobs)
 	cli := interfaces.NewCLI(treeService)
 
 	err := cli.Run(path)
